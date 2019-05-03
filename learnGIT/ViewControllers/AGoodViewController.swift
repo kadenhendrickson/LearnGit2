47911cd6f86a6a51a57cf1c59fc858c468b7d001
Kaden Hendrickson <kadenhendrickson4@gmail.com>
2019-05-03 16:25:39 -0600

    Added viewController

---
+++ learnGIT/ViewControllers/AGoodViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AGoodViewController.swift
+//  learnGIT
+//
+//  Created by Kaden Hendrickson on 5/3/19.
+//  Copyright © 2019 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class AGoodViewController: UIViewController {
+
+    @IBOutlet weak var TextLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
